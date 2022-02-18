a1bd0936303ae824085c6fa9dd37a9ad3315ea41
Julien Lambert <julien.lambert.perso@gmail.com>
2022-02-18 15:05:15 +0100

    [doc][TimeValue] add JSDoc (+ some adjustment)

--- src/TimeValue.ts
+++ src/TimeValue.ts
@@ -1,5 +1,13 @@
+/**
+ * All available Time units
+ */
 export type TimeUnits = "h" | "min" | "s" | "ms";
 
+/**
+ * Utils value
+ * bound is the max value (in a 'clock system')
+ * ms_rapport is the divider to use for convert millisecond to the actual unit
+ */
 const infos: UnitInfos = {
     h: { bound: +Infinity, ms_rapport: 1e3 * 60 * 60 },
     min: { bound: 60, ms_rapport: 1e3 * 60 },
@@ -7,11 +15,42 @@ const infos: UnitInfos = {
     ms: { bound: 1e3, ms_rapport: 1 },
 };
 
+/**
+ * TimeValue class
+ *
+ * Hold a raw time value in millisecond and can convert it in another unit (hour, minute, seconde)
+ *
+ * Instanciate it with `TimeValue.from()` passe the raw value and the unit of this raw value (think like `Buffer.from(str,encoding)`)
+ * ```js
+ * // raw value in millisecond
+ * const raw_value_in_ms = 354
+ * const value = TimeValue.from(raw_value_in_ms,"ms")
+ *
+ * // raw value in minutes
+ * const raw_value_in_min = 75
+ * const value = TimeValue.from(raw_value_in_min,"min")
+ * ```
+ */
 export class TimeValue {
-    private _baseValue = 0;
+    /**
+     *
+     *
+     * @private
+     *
+     * Hold the raw value in millisecond
+     */
+    private _rawValue = 0;
 
-    get basValue(): number {
-        return this._baseValue;
+    /**
+     *
+     *
+     * @readonly
+     * @type {number}
+     *
+     * Return the raw value
+     */
+    get rawValue(): number {
+        return this._rawValue;
     }
 
     private constructor(opt: TimeValueConstructor) {
@@ -19,12 +58,26 @@ export class TimeValue {
             const key = k as TimeUnits;
             const val = opt[key] || 0;
 
-            this._baseValue += val * infos[key].ms_rapport;
+            this._rawValue += val * infos[key].ms_rapport;
         });
     }
 
-    static from(opt: TimeValueConstructor): TimeValue;
+    /**
+     *
+     * Constructor, build a new TimeValue
+     *
+     * `TimeValue.from(value,[unit])`
+     * * `value` the initial value
+     * * `unit` the unit of the initial value (default `"ms"`)
+     *
+     * Or for more complexe situation, you can specify multiple composante:
+     *
+     * `TimeValue.from({h?:number,min?:number,s?:number,ms?:number})`
+     *
+     *
+     */
     static from(value: number, unit?: TimeUnits): TimeValue;
+    static from(opt: TimeValueConstructor): TimeValue;
     static from(value: TimeValueConstructor | number, unit: TimeUnits = "ms"): TimeValue {
         if (typeof value === "number") {
             return new TimeValue({ [unit]: value });
@@ -33,9 +86,42 @@ export class TimeValue {
         return new TimeValue(value);
     }
 
-    value(unit?: TimeUnits): number;
-    value(opt: ValueOpt): number;
-    value(opt: TimeUnits | ValueOpt | undefined): number {
+    /**
+     * Return the value in the specified unit
+     *
+     * ```js
+     * const initial = 1.245 // value in sec
+     * const time = TimeValue.from(initial,"s") // build the timevalue
+     *
+     * time.to("ms")
+     * // return the value in millisecond
+     * // output: 1245
+     * ```
+     *
+     * The returned value is simply the stored raw value (in `ms`) divided by the correct value (1000 for `sec`, 60000 for `min` and  3600000 for `h`)
+     *
+     * So the returned value can have a decimal part:
+     * `75 sec == 1.25 min`
+     *
+     * And can be 'out of bound':
+     * `65000 ms == 65 sec` but in clock system, sec are bound to 60 !
+     *
+     * You can adjust the returned value:
+     * ```
+     * time.to({
+     * 		unit?: "h" | "min" | "s" | "ms"
+     * 		decimal?: boolean | number | "trunc"
+     * 		bounded?: boolean
+     * })
+     * ```
+     *
+     * * default `unit`is `"ms"`
+     * * `decimal`: round the result to the specified number of decimal (0 if `false`) or truncate the result (get the interger part) if `"trunc"`
+     * * `bounded`: bound the value (ex: 65 sec == 5 sec if bounded)
+     */
+    to(unit?: TimeUnits): number;
+    to(opt: ValueOpt): number;
+    to(opt: TimeUnits | ValueOpt | undefined): number {
         if (opt === undefined) {
             return this.computeValue({ unit: "ms" });
         }
@@ -47,37 +133,67 @@ export class TimeValue {
         return this.computeValue(opt);
     }
 
-    decompose(opt: DecomposeOpt): DecomposeResponse {
-        const res: DecomposeResponse = {};
-
-        Object.keys(opt).forEach((k) => {
-            const key = k as TimeUnits;
-
-            const o = opt[key];
-            if (o) {
-                res[key] = this.computeValue(o);
+    /**
+     * Decompose the raw value into multiple unit
+     *
+     * It is exactly the same as call `to()` many time with different `unit`
+     *
+     * ```
+     * const ms = time.to("ms")
+     * const sec = time.to("s")
+     * const min = time.to("min")
+     * const hour = time.to("h")
+     *
+     * // or in one call
+     *
+     * const [hour,min,sec,ms] = time.decompose(["h","min","sec","ms"])
+     *
+     * ```
+     *
+     * You can use complexe option for adjust a value
+     * ```
+     * time.decompose([{unit:"min",bounded:true,decimal:4}])
+     * ```
+     * And passe the same unit many time
+     * ```
+     * time.decompose(["min",{unit:"min",bounded:true,decimal:4}])
+     * ```
+     */
+    decompose(opt: Array<TimeUnits | ValueOpt>): number[] {
+        return opt.map((o) => {
+            if (typeof o === "object") {
+                return this.to(o);
+            } else {
+                return this.to(o);
             }
         });
-
-        return res;
     }
 
     private computeValue(opt: ValueOpt): number {
+        // unit to use (default "ms")
         opt.unit = opt.unit || "ms";
-        let val = this._baseValue / infos[opt.unit].ms_rapport;
+        // compute the value in the correct unit
+        let val = this._rawValue / infos[opt.unit].ms_rapport;
 
+        // if bounded take the rest (%)
         if (opt.bounded) {
             val = val % infos[opt.unit].bound;
         }
 
+        // adjust decimal
         switch (typeof opt.decimal) {
             case "boolean":
+                // true ==> return full value
+                // false ==>  round to integer
                 val = opt.decimal ? val : Math.round(val);
                 break;
             case "number":
+                // keep only n decimal(s) (value is rounded)
                 val = Math.round(val * 10 ** opt.decimal) / 10 ** opt.decimal;
                 break;
             case "string":
+                // string == "trunc"
+                // keep only the integer part (truncate)
                 val = Math.trunc(val);
                 break;
 
@@ -103,6 +219,3 @@ interface ValueOpt {
     bounded?: boolean;
     decimal?: boolean | number | "trunc";
 }
-
-type DecomposeOpt = Partial<Record<TimeUnits, ValueOpt>>;
-type DecomposeResponse = Partial<Record<TimeUnits, number>>;
